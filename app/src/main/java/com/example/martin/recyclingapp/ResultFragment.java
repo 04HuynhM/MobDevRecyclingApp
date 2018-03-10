4d4941150a26e05d64d8e0291267982ba33ce478
Martin <martin.huynh0055@stud.hkr.se>
2018-03-10 17:37:32 +0100

    Adapted camera activity
    Camera activity now works in new main class

---
+++ app/src/main/java/com/example/martin/recyclingapp/ResultFragment.java
@@ -0,0 +1,23 @@
+package com.example.martin.recyclingapp;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Martin on 11/02/2018.
+ */
+
+public class ResultFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_result, container, false);
+
+        return view;
+    }
+}
