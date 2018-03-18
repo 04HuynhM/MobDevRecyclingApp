bd913f012a017c4dcaf598cd203f7fec7bfbd090
Martin Huynh <martinhuynh@iMac.local>
2018-03-18 18:28:44 +0100

    History and results implemented
    Results and history partially implemented, waiting for
    database to complete.

--- app/src/main/java/com/example/martin/recyclingapp/db/Item.java
+++ app/src/main/java/com/example/martin/recyclingapp/db/Item.java
@@ -1,5 +1,7 @@
 package com.example.martin.recyclingapp.db;
 
+import java.util.Date;
+
 /**
  * Created by charlie on 2018-03-10.
  */
@@ -7,4 +9,39 @@ package com.example.martin.recyclingapp.db;
 public class Item {
     private String title;
     private String description;
+    private String productName;
+    private String barcodeNumber;
+    private String productMaterial;
+    private Date dateScanned;
+
+    public Item(String barcodeNumber, String productName, String productMaterial, Date dateScanned){
+        this.barcodeNumber = barcodeNumber;
+        this.productName = productName;
+        this.productMaterial = productMaterial;
+        this.dateScanned = dateScanned;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public String getBarcodeNumber() {
+        return barcodeNumber;
+    }
+
+    public String getProductMaterial() {
+        return productMaterial;
+    }
+
+    public Date getDateScanned() {
+        return dateScanned;
+    }
 }
--- app/src/main/java/com/example/martin/recyclingapp/history/HistoryFragment.java
+++ app/src/main/java/com/example/martin/recyclingapp/history/HistoryFragment.java
@@ -1,13 +1,19 @@
 package com.example.martin.recyclingapp.history;
 
 import android.app.Fragment;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.martin.recyclingapp.R;
+import com.example.martin.recyclingapp.db.Item;
+
+import java.util.ArrayList;
 
 /**
  * Created by charlie on 2018-02-02.
@@ -15,10 +21,18 @@ import com.example.martin.recyclingapp.R;
 
 public class HistoryFragment extends Fragment {
 
+    ArrayList<Item> items;
+    private RecyclerView historyList;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+
         View view = inflater.inflate(R.layout.fragment_history, container, false);
+
+        historyList = view.findViewById(R.id.historyList);
+
+
 
         return view;
     }
