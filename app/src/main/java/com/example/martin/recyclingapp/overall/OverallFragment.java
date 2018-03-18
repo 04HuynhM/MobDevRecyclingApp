e053308e9aa9d3f0a6f391d21a63522c3b17fee6
Martin Huynh <martinhuynh@iMac.local>
2018-03-19 00:39:18 +0100

    Added Facebook Login Functionality
    Added facebook login button to LoginActivity as well as login logic.
    Added temporary log out button to OverallFragment. Button now logs out
    of both facebook and firebase.
    Facebook login is paired with firebase server so they work together.

--- app/src/main/java/com/example/martin/recyclingapp/overall/OverallFragment.java
+++ app/src/main/java/com/example/martin/recyclingapp/overall/OverallFragment.java
@@ -1,7 +1,10 @@
 package com.example.martin.recyclingapp.overall;
 
+import android.app.AlertDialog;
 import android.app.Fragment;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -9,9 +12,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.martin.recyclingapp.R;
 import com.example.martin.recyclingapp.sign.LoginActivity;
+import com.facebook.login.LoginManager;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 /**
  * Created by charlie on 2018-02-02.
@@ -23,15 +30,58 @@ public class OverallFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_overall, container, false);
+        TextView textView = view.findViewById(R.id.text_overall);
 
         // Test purposes, will be removed
         Button loginButton = view.findViewById(R.id.button_login_test_overall);
+        Button logoutButton = view.findViewById(R.id.button_log_out_test_overall);
         loginButton.setOnClickListener(v -> {
-            Intent intent = new Intent(getActivity(), LoginActivity.class);
-            getActivity().startActivity(intent);
+            Intent intent = new Intent(OverallFragment.this.getActivity(), LoginActivity.class);
+            OverallFragment.this.getActivity().startActivity(intent);
         });
 
-        TextView textView = view.findViewById(R.id.text_overall);
+        logoutButton.setOnClickListener(v -> {
+
+            if(FirebaseAuth.getInstance().getCurrentUser() != null) {
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+                AlertDialog dialog = builder.setTitle("Log Out")
+                        .setMessage("Are you sure you want to log out?")
+                        .setPositiveButton("Log Out", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                FirebaseAuth.getInstance().signOut();
+                                LoginManager.getInstance().logOut();
+                                textView.setText("");
+
+                            }
+                        })
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                dialogInterface.dismiss();
+                            }
+                        })
+                        .create();
+
+                dialog.setOnShowListener(new DialogInterface.OnShowListener() {
+                    @Override
+                    public void onShow(DialogInterface dialogInterface) {
+                        dialog.getButton(DialogInterface.BUTTON_NEGATIVE).setTextColor(Color.BLACK);
+                        dialog.getButton(DialogInterface.BUTTON_POSITIVE).setTextColor(Color.BLACK);
+                    }
+                });
+
+                dialog.show();
+            }
+            else {
+                Toast.makeText(getActivity(), "You are not signed in.", Toast.LENGTH_LONG).show();
+            }
+
+        });
+
+
         textView.setText(getArguments().getString("uid"));
 
         return view;
--- app/src/main/java/com/example/martin/recyclingapp/sign/LoginActivity.java
+++ app/src/main/java/com/example/martin/recyclingapp/sign/LoginActivity.java
@@ -1,29 +1,54 @@
 package com.example.martin.recyclingapp.sign;
 
 import android.app.Fragment;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.martin.recyclingapp.MainActivity;
 import com.example.martin.recyclingapp.R;
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthCredential;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class LoginActivity extends AppCompatActivity {
+
+    CallbackManager callbackManager;
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuth = FirebaseAuth.getInstance();
 
         if (firebaseAuth.getCurrentUser() != null) {
             Intent intent = new Intent(this, MainActivity.class);
@@ -35,14 +60,15 @@ public class LoginActivity extends AppCompatActivity {
         EditText passEdit = findViewById(R.id.edit_password_login);
         Button signInButton = findViewById(R.id.button_sign_in_login);
         TextView registerText = findViewById(R.id.text_button_register_login);
+        LoginButton fbLoginButton = findViewById(R.id.facebook_login_button);
 
         signInButton.setOnClickListener(v -> {
             firebaseAuth.signInWithEmailAndPassword(emailEdit.getText().toString().trim(), passEdit.getText().toString().trim())
                     .addOnCompleteListener(this, task -> {
-                        if(task.isSuccessful()) {
-                            Intent intent = new Intent(this, MainActivity.class);
+                        if (task.isSuccessful()) {
+                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                             intent.putExtra("uid", firebaseAuth.getCurrentUser().getUid());
-                            startActivity(intent);
+                            LoginActivity.this.startActivity(intent);
                         }
                     });
         });
@@ -54,5 +80,79 @@ public class LoginActivity extends AppCompatActivity {
                     .addToBackStack(null)
                     .commit()
         );
+
+        callbackManager = CallbackManager.Factory.create();
+        fbLoginButton.setReadPermissions("email", "public_profile");
+        fbLoginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                handleFacebookAccessToken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+
+
+        // USE THIS CODE TO GET YOUR HASH KEY AND ADD TO FACEBOOK DEVELOPER CONFIG
+//        try {
+//            PackageInfo info = getPackageManager().getPackageInfo(
+//                    "com.example.martin.recyclingapp",
+//                    PackageManager.GET_SIGNATURES);
+//            for (Signature signature : info.signatures) {
+//                MessageDigest md = MessageDigest.getInstance("SHA");
+//                md.update(signature.toByteArray());
+//                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+//            }
+//        } catch (PackageManager.NameNotFoundException e) {
+//
+//        } catch (NoSuchAlgorithmException e) {
+//
+//        }
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void handleFacebookAccessToken(AccessToken token) {
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        firebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()) {
+
+                    FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    intent.putExtra("uid", user.getUid());
+                    startActivity(intent);
+
+                }
+                else {
+                    Toast.makeText(LoginActivity.this, "Authentication Failed",
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
