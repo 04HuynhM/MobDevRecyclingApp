6d3ecbf6ba3f49e632c6b77be91531da32ade1b8
Martin Huynh <martin.huynh0055@stud.hkr.se>
2018-03-19 09:38:12 +0100

    Update ResultFragment.java

--- app/src/main/java/com/example/martin/recyclingapp/ResultFragment.java
+++ app/src/main/java/com/example/martin/recyclingapp/ResultFragment.java
@@ -16,23 +16,9 @@ public class ResultFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-<<<<<<< Updated upstream
         View view = inflater.inflate(R.layout.fragment_result, container, false);
-=======
-<<<<<<< Updated upstream
-        View view = inflater.inflate(R.layout.fragment_result2, container, false);
-=======
-
-        View view = inflater.inflate(R.layout.fragment_result, container, false);
-
         Bundle bundle = getArguments();
         String scannedBarcode = bundle.getString("barcode");
-
-
-
-
->>>>>>> Stashed changes
->>>>>>> Stashed changes
         return view;
     }
 
