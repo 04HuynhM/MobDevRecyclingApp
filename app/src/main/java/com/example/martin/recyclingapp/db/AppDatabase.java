f87bdc41fe7f432b390bbdee989d7efeee5e4d02
CharlieTunasson <charlie@tunasson.com>
2018-03-24 19:38:54 +0100

    Login and register UI and Error Handling
    Login and register UIs finished and Error handling
    added. (Facebook not finished)

--- app/src/main/java/com/example/martin/recyclingapp/db/AppDatabase.java
+++ app/src/main/java/com/example/martin/recyclingapp/db/AppDatabase.java
@@ -14,14 +14,13 @@ import com.google.firebase.database.ValueEventListener;
  * Created by charlie on 2018-03-18.
  */
 
-@Database(entities =  {User.class, Item.class}, version = 1)
+@Database(entities =  {Item.class}, version = 1)
 public abstract class AppDatabase extends RoomDatabase {
 
     private static final String TAG = "AppDatabase";
 
     private static AppDatabase INSTANCE;
 
-    public abstract UserDao userDao();
     public abstract ItemDao itemDao();
 
     public static AppDatabase getAppDatabase(Context context) {
